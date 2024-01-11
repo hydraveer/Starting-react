e28df4b74ebbc2202b38b29561ab4a4788e6be0a
hydraveer <veerendrapatel420@gmail.com>
2024-01-11 17:13:20 +0530

    Todo Application is created using LocalStorage

---
+++ 10TodoWithLocalStorage/src/App.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import { TodoProvider } from "./Contexts/TodoContext"
+import TodoForm from "./Component/TodoForm"
+import TodoItem from "./Component/TodoItem"
+
+
+function App() {
+ const [todos,setTodo]=useState([])
+
+  const addTodo=(todo)=>{
+    setTodo((prev)=>[{id:Date.now()
+    ,...todo},...prev])
+  }
+  const updateTodo=(todo,id)=>{
+    setTodo((prev)=>prev.map((prevTodo)=>(prevTodo.id===id? todo : prevTodo)))
+  }
+
+  const deleteTodo=(id)=>{
+    setTodo((prev)=>prev.filter((todo)=>todo.id!==id))
+  }
+
+  const toggleComplete=(id)=>{
+    setTodo((prev)=>
+    prev.map((prevTodo)=>
+    prevTodo.id===id?
+    {...prevTodo,completed:!prevTodo.completed}
+    : prevTodo))
+  }
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todo"));
+  
+    if (storedTodos && storedTodos.length > 0) {
+      setTodo(storedTodos);
+    }
+  }, []);
+  
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todos));
+  }, [todos]);
+  
+  return (
+         <TodoProvider value={{todos, addTodo,updateTodo,deleteTodo,toggleComplete}}>
+            <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        <TodoForm/>
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {todos.map((todo)=>(
+                          <div key={todo.id} className="w-full">
+                            <TodoItem todo={todo}/>
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+          </TodoProvider>
+  )
+}
+
+export default App
